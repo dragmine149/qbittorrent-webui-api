82efad9330d2c5bff99f03cab0e58a57f3e5ba4d
mattress <mattress@mattress.no>
2025-06-09 12:51:32 +0200

    Error handeling

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,10 @@
 use core::str;
-use std::{io::Error, str::FromStr};
+use std::{str::FromStr};
 
 use reqwest::{Client as ReqwestClient, Url};
 use tokio::sync::RwLock;
+
+use crate::error::Error;
 
 pub struct Creddentials {
     username: String,
@@ -16,14 +18,11 @@ pub struct Client {
 
 impl Client {
     pub async fn new(url: &str) -> Result<Self, Error> {
-        let http_client = ReqwestClient::builder()
-            .cookie_store(true)
-            .build()
-            .unwrap();
+        let http_client = ReqwestClient::builder().cookie_store(true).build().unwrap();
 
         let base_url = Url::from_str(url).unwrap();
 
-        Ok(Self { 
+        Ok(Self {
             http_client: http_client,
             base_url: RwLock::new(base_url),
         })
@@ -31,30 +30,29 @@ impl Client {
 
     async fn build_url(&self, endpoint: &str) -> Result<Url, Error> {
         let base_url = self.base_url.read().await;
-        let url = base_url.join(endpoint).unwrap(); 
-        
+        let url = base_url.join(endpoint)?;
+
         Ok(url)
     }
 
     pub async fn login(&self, cred: Creddentials) -> Result<(), Error> {
-        let url = self.build_url("/api/v2/auth/login").await.unwrap();
+        let url = self.build_url("/api/v2/auth/login").await?;
 
         let res = self
             .http_client
             .post(url)
-            .body(format!("username={}&password={}", cred.username, cred.password))
+            .body(format!(
+                "username={}&password={}",
+                cred.username, cred.password
+            ))
             .header("refer", self.base_url.read().await.to_string())
             .send()
-            .await
-            .unwrap();
+            .await?;
 
         if res.status().is_success() {
             Ok(())
         } else {
-            Err(Error::new(
-                std::io::ErrorKind::PermissionDenied,
-                "Login failed",
-            ))
+            Err(Error::AuthFailed)
         }
     }
 
---
+++ src/error.rs
@@ -0,0 +1,21 @@
+
+
+#[derive(Debug)]
+pub enum Error {
+    AuthFailed,
+    HttpRequestError(reqwest::Error),
+    ParseError(url::ParseError)
+}
+
+
+impl From<reqwest::Error> for Error {
+    fn from(value: reqwest::Error) -> Self {
+        Self::HttpRequestError(value)
+    }
+}
+
+impl From<url::ParseError> for Error {
+    fn from(value: url::ParseError) -> Self {
+        Self::ParseError(value)   
+    }
+}
