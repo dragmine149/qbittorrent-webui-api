b64694191d3ee6c3176c8e8ed428c1631cd91df3
mattress <mattress@mattress.no>
2025-06-23 18:04:11 +0200

    added RSS add feed

--- src/client/rss.rs
+++ src/client/rss.rs
@@ -18,4 +18,23 @@ impl super::Api {
 
         Ok(())
     }
+
+    /// Add RSS feed
+    ///
+    /// # Arguments
+    /// * `feed_url` - URL of RSS feed (e.g. "http://thepiratebay.org/rss//top100/200")
+    /// * `path` - Full path of added folder (e.g. "The Pirate Bay\Top100")
+    pub async fn rss_add_feed(&self, feed_url: &str, path: Option<&str>) -> Result<(), Error> {
+        let url = self._build_url("api/v2/rss/addFeed").await?;
+
+        let mut form = multipart::Form::new();
+        form = form.text("feed_url", feed_url.to_string());
+        if let Some(path) = path {
+            form = form.text("path", path.to_string());
+        }
+
+        self.http_client.post(url).multipart(form).send().await?;
+
+        Ok(())
+    }
 }
