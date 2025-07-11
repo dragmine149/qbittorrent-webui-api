df55bc98d6d2c4a3604b01a0d0098c666cb824f1
Dragmine149 <dan@thebanners.uk>
2025-07-11 19:11:25 +0100

    Added another test for more url validation

--- tests/authentication/login_user_pass.rs
+++ tests/authentication/login_user_pass.rs
@@ -1,4 +1,5 @@
-use qbit::Api;
+use qbit::{Api, Error};
+use url::ParseError::RelativeUrlWithoutBase;
 
 #[tokio::test]
 async fn login_url_1() {
@@ -14,6 +15,16 @@ async fn login_url_2() {
         Api::new_login_username_password("http://127.0.0.1:8090", "admin", "torrent").await;
 
     assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn login_url_3() {
+    let result = Api::new_login_username_password("127.0.0.1:8090", "admin", "torrent").await;
+
+    assert!(result.is_err());
+    let err = result.err().unwrap();
+
+    assert!(matches!(err, Error::InvalidURL(RelativeUrlWithoutBase)));
 }
 
 #[tokio::test]
