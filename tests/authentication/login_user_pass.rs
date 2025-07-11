aacca51e5b244e6fd4c3d39e714d752b98bf73bf
Dragmine149 <dan@thebanners.uk>
2025-07-11 20:34:29 +0100

    Added cache and fix default password

---
+++ tests/authentication/login_user_pass.rs
@@ -0,0 +1,34 @@
+use qbit::Api;
+
+#[tokio::test]
+async fn login_url_1() {
+    let result =
+        Api::new_login_username_password("http://127.0.0.1:8090/", "admin", "adminadmin").await;
+
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn login_url_2() {
+    let result =
+        Api::new_login_username_password("http://127.0.0.1:8090", "admin", "adminadmin").await;
+
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn incorrect_username() {
+    let result =
+        Api::new_login_username_password("http://127.0.0.1:8090/", "fjiooiaaso", "adminadmin")
+            .await;
+
+    assert!(result.is_err());
+}
+
+#[tokio::test]
+async fn incorrect_password() {
+    let result =
+        Api::new_login_username_password("http://127.0.0.1:8090/", "admin", "snkabjhioahsio").await;
+
+    assert!(result.is_err());
+}
