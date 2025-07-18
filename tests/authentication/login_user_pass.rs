dad42ad3d4ae6d827db3cb33a4d2e8ea087e0de9
Dragmine149 <dan@thebanners.uk>
2025-07-18 22:29:12 +0100

    chore: removed tests that were included in #7

--- tests/authentication/login_user_pass.rs
+++ tests/authentication/login_user_pass.rs
@@ -1,30 +1,6 @@
 use qbit::Api;
 
 #[tokio::test]
-async fn login_url_1() {
-    let result =
-        Api::new_login_username_password("http://localhost:45378/", "admin", "adminadmin").await;
-
-    if result.is_err() {
-        eprintln!("{:?}", result.as_ref().err().unwrap());
-    }
-
-    assert!(result.is_ok());
-}
-
-#[tokio::test]
-async fn login_url_2() {
-    let result =
-        Api::new_login_username_password("http://localhost:45378", "admin", "adminadmin").await;
-
-    if result.is_err() {
-        eprintln!("{:?}", result.as_ref().err().unwrap());
-    }
-
-    assert!(result.is_ok());
-}
-
-#[tokio::test]
 async fn incorrect_username() {
     let result =
         Api::new_login_username_password("http://localhost:45378/", "fjiooiaaso", "adminadmin")
