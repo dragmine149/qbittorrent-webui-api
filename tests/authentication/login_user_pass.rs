517a5217d295dda3696d7b6df0429ef8ea4aa402
Dragmine149 <dan@thebanners.uk>
2025-07-18 22:32:29 +0100

    Added another test to make sure that github cli test suite works

--- tests/authentication/login_user_pass.rs
+++ tests/authentication/login_user_pass.rs
@@ -1,6 +1,19 @@
 use qbit::Api;
 
 #[tokio::test]
+async fn correct_credentials() {
+    let result =
+        Api::new_login_username_password("http://localhost:45378/", "admin", "adminadmin").await;
+
+    if result.is_err() {
+        println!("Err: {:?}", result.err().unwrap());
+        assert!(false);
+    }
+
+    assert!(true);
+}
+
+#[tokio::test]
 async fn incorrect_username() {
     let result =
         Api::new_login_username_password("http://localhost:45378/", "fjiooiaaso", "adminadmin")
