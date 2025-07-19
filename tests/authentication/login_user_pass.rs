5874a3073f2cefd7d4fae74f05c298b1b51db3f5
Dragmine149 <dan@thebanners.uk>
2025-07-19 14:40:02 +0100

    refactor: server details so that other future tests can use them

--- tests/authentication/login_user_pass.rs
+++ tests/authentication/login_user_pass.rs
@@ -1,30 +1,5 @@
-use dotenv::dotenv;
+use crate::{get_server_details, get_server_password, get_server_username};
 use qbit::Api;
-use std::env;
-
-fn get_server_details() -> String {
-    dotenv().ok();
-
-    let url = env::var("url");
-    let port = env::var("port");
-
-    if url.is_err() || port.is_err() {
-        println!("Default to `http://localhost:45378` as couldn't fully load data from .env");
-        return String::from("http://localhost:45378");
-    }
-
-    let finished_url = format!("{}:{}", url.unwrap(), port.unwrap());
-    println!("Using {} from .env file", finished_url);
-    finished_url
-}
-
-fn get_server_username() -> String {
-    env::var("username").unwrap_or("admin".to_string())
-}
-
-fn get_server_password() -> String {
-    env::var("password").unwrap_or("adminadmin".to_string())
-}
 
 #[tokio::test]
 #[ignore = "Test hits api endpoint"]
