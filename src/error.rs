23c098261c92a606e732cd8b5153ba8494dca8cc
DrRac27 <drrac27@riseup.net>
2025-07-25 13:03:37 +0200

    Implement std::error::Error for qbit::Error

--- src/error.rs
+++ src/error.rs
@@ -29,3 +29,11 @@ impl From<url::ParseError> for Error {
         Self::UrlParseError(err)
     }
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self}")
+    }
+}
+
+impl std::error::Error for Error {}
