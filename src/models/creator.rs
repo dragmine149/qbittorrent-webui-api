a8928f789f54bdba66d31c8acdc0370921238ec7
Dragmine149 <dan@thebanners.uk>
2025-08-13 17:00:25 +0100

    Added templates for piece size

--- src/models/creator.rs
+++ src/models/creator.rs
@@ -42,3 +42,79 @@ pub struct TorrentCreator {
 pub struct TorrentCreatorTask {
     pub task_id: String,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct TorrentPieceSize(u64);
+
+impl Default for TorrentPieceSize {
+    fn default() -> Self {
+        Self::auto()
+    }
+}
+
+impl TorrentPieceSize {
+    /// Leave it up to qbittorrent to decided.
+    pub fn auto() -> Self {
+        Self(0)
+    }
+    /// Pieces in 16 KibiBytes (16 * 1024)
+    pub fn k16() -> Self {
+        Self(16384)
+    }
+    /// Pieces in 32 KibiBytes (32 * 1024)
+    pub fn k32() -> Self {
+        Self(32768)
+    }
+    /// Pieces in 64 KibiBytes (64 * 1024)
+    pub fn k64() -> Self {
+        Self(65536)
+    }
+    /// Pieces in 128 KibiBytes (128 * 1024)
+    pub fn k128() -> Self {
+        Self(131072)
+    }
+    /// Pieces in 256 KibiBytes (256 * 1024)
+    pub fn k256() -> Self {
+        Self(262144)
+    }
+    /// Pieces in 512 KibiBytes (512 * 1024)
+    pub fn k512() -> Self {
+        Self(524288)
+    }
+    /// Pieces in 1 MebiBytes (1 * 1024 * 1024)
+    pub fn m1() -> Self {
+        Self(1048576)
+    }
+    /// Pieces in 2 MebiBytes (2 * 1024 * 1024)
+    pub fn m2() -> Self {
+        Self(2097152)
+    }
+    /// Pieces in 4 MebiBytes (4 * 1024 * 1024)
+    pub fn m4() -> Self {
+        Self(4194304)
+    }
+    /// Pieces in 8 MebiBytes (8 * 1024 * 1024)
+    pub fn m8() -> Self {
+        Self(8388608)
+    }
+    /// Pieces in 16 MebiBytes (16 * 1024 * 1024)
+    pub fn m16() -> Self {
+        Self(16777216)
+    }
+    /// Pieces in 32 MebiBytes (32 * 1024 * 1024)
+    pub fn m32() -> Self {
+        Self(33554432)
+    }
+    /// Pieces in 64 MebiBytes (64 * 1024 * 1024)
+    pub fn m64() -> Self {
+        Self(67108864)
+    }
+    /// Pieces in 128 MebiBytes (128 * 1024 * 1024)
+    pub fn m128() -> Self {
+        Self(134217728)
+    }
+    /// Pieces in 256 MebiBytes (256 * 1024 * 1024)
+    pub fn m256() -> Self {
+        Self(268435456)
+    }
+}
